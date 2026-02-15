d7059765ecec1bb21c1b4432414295e5380ac328
Palak <sharma.palak2455@gmail.com>
2026-02-15 10:23:44 +0530

    Day 28 of Leetcode

---
+++ Easy/Linked List Cycle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        if(head == NULL) return false;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast != NULL && fast->next != NULL) {
+            
+            slow = slow->next;          // move 1 step
+            fast = fast->next->next;    // move 2 steps
+            
+            if(slow == fast) {
+                return true;            // cycle detected
+            }
+        }
+        
+        return false;   // no cycle
+    }
+};
