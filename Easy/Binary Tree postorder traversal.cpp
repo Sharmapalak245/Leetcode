81931dacf18e1bbae034d8ce63522e2b6b038598
Palak <sharma.palak2455@gmail.com>
2026-02-18 13:53:40 +0530

    Day 29 of Leetcode

---
+++ Easy/Binary Tree postorder traversal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void traverse(TreeNode*root,vector<int>& res){
+        if(root==nullptr){
+            return;
+        }
+
+        traverse(root->left,res);
+        traverse(root->right,res);
+        res.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+            vector<int> res;
+            traverse(root,res);
+            return res;
+    }
+};
